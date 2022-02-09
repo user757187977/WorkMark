0201e15fb40c92b84f5a42e27dbceafa897aa231
spli <lishoupeng@zhihu.com>
2022-02-09 09:21:27 +0800

    feat: 数组专栏

--- src/codetop/数组/三数之和.java
+++ src/codetop/数组/三数之和.java
@@ -12,10 +12,9 @@ import java.util.List;
 public class 三数之和 {
 
     /**
-     * 先排序, 原数组: -1,0,1,2,-1,-4 -> 新数组: -4,-2,-1,-1,0,1,2
-     * 开始遍历, 第一个元素 -4, 我们只需要找后面两个数相加等于 4 的就可以了, 这时候借助双指针
-     * 这样从原来的三个数相加等于 0 变成了找两个数相加等于 4
-     * @return
+     * 1.先排序
+     * 2.遍历, 第一个元素 -4, 我们只需要找后面两个数相加等于 4 的就可以了
+     * 3.双指针, 这样从原来的三个数相加等于 0 变成了找两个数相加等于 4
      */
     public static List<List<Integer>> test(int[] nums) {// 总时间复杂度：O(n^2)
         List<List<Integer>> ans = new ArrayList<>();
@@ -27,13 +26,14 @@ public class 三数之和 {
             if (nums[i] > 0) break; // 第一个数大于 0，后面的数都比它大，肯定不成立了
             if (i > 0 && nums[i] == nums[i - 1]) continue; // 去掉重复情况
             int target = -nums[i];
-            int left = i + 1, right = nums.length - 1;
+            int left   = i + 1, right = nums.length - 1;
             while (left < right) {
                 if (nums[left] + nums[right] == target) {
                     ans.add(new ArrayList<>(Arrays.asList(nums[i], nums[left], nums[right])));
 
                     // 现在要增加 left，减小 right，但是不能重复，比如: [-2, -1, -1, -1, 3, 3, 3], i = -2, left = 1, right = 6, [-2, -1, 3] 的答案加入后，需要排除重复的 -1 和 3
-                    left++; right--; // 首先无论如何先要进行加减操作
+                    left++;
+                    right--; // 首先无论如何先要进行加减操作
                     while (left < right && nums[left] == nums[left - 1]) left++;
                     while (left < right && nums[right] == nums[right + 1]) right--;
                 } else if (nums[left] + nums[right] < target) {
@@ -47,7 +47,7 @@ public class 三数之和 {
     }
 
     public static void main(String[] args) {
-        test(new int[]{-1,0,1,2,-1,-4});
+        test(new int[]{-1, 0, 1, 2, -1, -4});
     }
 
 }
