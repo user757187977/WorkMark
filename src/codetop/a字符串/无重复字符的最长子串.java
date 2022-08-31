a5c59808b24cc04b32365850270efdec60168ea2
spli <lishoupeng@zhihu.com>
2022-08-31 10:05:15 +0800

    feat: 滑动窗口思路

--- src/codetop/a字符串/无重复字符的最长子串.java
+++ src/codetop/a字符串/无重复字符的最长子串.java
@@ -8,7 +8,7 @@ import java.util.Set;
  * @Author spli
  * @Date 2022/1/31 10:55
  * 输入: s = "abcabcbb"
- * 输出: 3
+ * 输出: 3 或者 abc
  * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3
  * 从 a 开始, 分别出现了 a, ab, abc, abcb 但是 abcb 有两个 b, 有重复, 所以从 a 开始的最长无重复子串是 abc
  * 从 b 开始, 分别出现了 b, bc, bca, bcab 但是 bcab 有两个 b, 有重复, 所以从 b 开始的最长无重复子串是 bca
@@ -16,7 +16,7 @@ import java.util.Set;
 public class 无重复字符的最长子串 {
 
     public static String test(String s) {
-        String result = null;
+        String result = "";
         if (null == s || "".equals(s)) {
             return null;
         }
@@ -36,13 +36,16 @@ public class 无重复字符的最长子串 {
                     temps.append(temp_j);
                 }
             }
-            System.out.println(temps);
+//            System.out.println(temps);
+            if (temps.toString().length() > result.length()) {
+                result = temps.toString();
+            }
             check.clear();
         }
         return result;
     }
 
     public static void main(String[] args) {
-        test("abcabcbb");
+        System.out.println(test("abcabcbb"));
     }
 }
