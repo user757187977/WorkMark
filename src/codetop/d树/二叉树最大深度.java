290b1a92a972d4f665670c733412e39ee927c4eb
spli <lishoupeng@zhihu.com>
2022-09-07 09:24:30 +0800

    feat: 二叉树深度

--- src/codetop/d树/二叉树最大深度.java
+++ src/codetop/d树/二叉树最大深度.java
@@ -1,6 +1,32 @@
-package codetop.d树;/**
- * @Description  
- * @Author       lishoupeng
- * @Date         2022/9/7 09:10
- */public class 二叉树最大深度 {
+package codetop.d树;
+
+import 数据结构.TreeNode;
+
+/**
+ * @Description https://leetcode.cn/problems/maximum-depth-of-binary-tree/solution/er-cha-shu-de-zui-da-shen-du-by-leetcode-solution/
+ * @Author lishoupeng
+ * @Date 2022/9/7 09:10
+ */
+public class 二叉树最大深度 {
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftHeight = maxDepth(root.left);
+            int rightHeight = maxDepth(root.right);
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(1);
+        TreeNode treeNode1 = new TreeNode(2);
+        TreeNode treeNode2 = new TreeNode(3);
+        TreeNode treeNode3 = new TreeNode(4);
+        treeNode.left = treeNode1;
+        treeNode.right = treeNode2;
+        treeNode1.left = treeNode3;
+        System.out.println(maxDepth(treeNode));
+    }
 }
