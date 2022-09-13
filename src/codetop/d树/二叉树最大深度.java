649e5710a19177a6c6976ebd9e887d3fa6a27dab
spli <lishoupeng@zhihu.com>
2022-09-13 09:35:59 +0800

    feat: 二叉树深度

--- src/codetop/d树/二叉树最大深度.java
+++ src/codetop/d树/二叉树最大深度.java
@@ -13,20 +13,22 @@ public class 二叉树最大深度 {
         if (root == null) {
             return 0;
         } else {
+            System.out.println("当前的 root:" + root.val + " 开始递归左节点");
             int leftHeight = maxDepth(root.left);
+            System.out.println("当前的 root:" + root.val + " 开始递归右节点");
             int rightHeight = maxDepth(root.right);
             return Math.max(leftHeight, rightHeight) + 1;
         }
     }
 
     public static void main(String[] args) {
-        TreeNode treeNode = new TreeNode(1);
-        TreeNode treeNode1 = new TreeNode(2);
-        TreeNode treeNode2 = new TreeNode(3);
-        TreeNode treeNode3 = new TreeNode(4);
-        treeNode.left = treeNode1;
-        treeNode.right = treeNode2;
+        TreeNode treeNode0 = new TreeNode(0);
+        TreeNode treeNode1 = new TreeNode(1);
+        TreeNode treeNode2 = new TreeNode(2);
+        TreeNode treeNode3 = new TreeNode(3);
+        treeNode0.left = treeNode1;
+        treeNode0.right = treeNode2;
         treeNode1.left = treeNode3;
-        System.out.println(maxDepth(treeNode));
+        System.out.println("二叉树的最大深度:" + maxDepth(treeNode0));
     }
 }
