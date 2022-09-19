86257e0182effe5e68e3c24faf4bdddaf8529a42
spli <lishoupeng@zhihu.com>
2022-09-19 09:39:16 +0800

    feat: hot100 数组类

---
+++ src/hot100/数组/三数之和.java
@@ -0,0 +1,43 @@
+package hot100.数组;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Description https://leetcode.cn/problems/3sum/
+ * @Author lishoupeng
+ * @Date 2022/9/19 08:32
+ * 输入：nums = [-1,0,1,2,-1,-4]
+ * 输出：[[-1,-1,2],[-1,0,1]]
+ */
+public class 三数之和 {
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> asw = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int target = -nums[i];
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] == target) {
+                    asw.add(new ArrayList<>(Arrays.asList(nums[i], nums[left], nums[right])));
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                } else if (nums[left] + nums[right] < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return asw;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+    }
+}
