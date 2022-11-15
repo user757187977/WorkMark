91a967af71cf92998ff3ce69d1eaae3c656e2956
spli <lishoupeng@zhihu.com>
2022-11-15 09:00:29 +0800

    feat: demo

--- src/code/hot100/d树/树的公共祖先.java
+++ src/code/hot100/d树/树的公共祖先.java
@@ -2,6 +2,11 @@ package code.hot100.d树;
 
 import code.数据结构.TreeNode;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * @Description https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-tree/comments/
  * @Author lishoupeng
@@ -9,6 +14,7 @@ import code.数据结构.TreeNode;
  */
 public class 树的公共祖先 {
 
+    // 方法一
     public static TreeNode dfs(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || root == p || root == q) return root;
         TreeNode left = dfs(root.left, p, q);
@@ -18,6 +24,36 @@ public class 树的公共祖先 {
         if (right == null) return left;
         return root;
     }
+
+    // 方法二
+    static Map<Integer, TreeNode> parent = new HashMap<>(); //K: 当前节点 V: 父节点
+    public static void dfs(TreeNode root) {
+        if (root.left != null) {
+            parent.put(root.left.val, root);
+            dfs(root.left);
+        }
+        if (root.right != null) {
+            parent.put(root.right.val, root);
+            dfs(root.right);
+        }
+    }
+
+    static Set<Integer> visited = new HashSet<>(); //保存了 p 递归向上的父节点
+    public static TreeNode commonGrand(TreeNode root, TreeNode p, TreeNode q) {
+        dfs(root);
+        while (p != null) {
+            visited.add(p.val);
+            p = parent.get(p.val);
+        }
+        while (q != null) {
+            if (visited.contains(q.val)) {
+                return q;
+            }
+            q = parent.get(q.val);
+        }
+        return null;
+    }
+
 
     public static void main(String[] args) {
         TreeNode treeNode1 = new TreeNode(1);
@@ -29,8 +65,10 @@ public class 树的公共祖先 {
         treeNode1.right = treeNode3;
         treeNode2.left = treeNode4;
         treeNode2.right = treeNode5;
-        TreeNode asw = dfs(treeNode1, treeNode4, treeNode5);
-        System.out.println(asw.val);
+        // 方法一
+        System.out.println(dfs(treeNode1, treeNode4, treeNode5).val);
+        // 方法二
+        System.out.println(commonGrand(treeNode1, treeNode4, treeNode5).val);
     }
 
 }
